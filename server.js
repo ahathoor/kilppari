4d347343bcfc8eaa63cbe612349b1765cc6bb637
Mikko Tamminen <mikko.k.tamminen@gmail.com>
2014-03-28 21:50:19 +0000

    added saving to file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var fs = require('fs');
 var codes = {};
-var index = 0;
+var nextIndex = 0;
 var app = express();
 
 app.use(logfmt.requestLogger());
@@ -12,33 +13,65 @@ app.use("/js", express.static(__dirname + "/public_html/js"));
 //   res.sendFile('public_html/' + req.query.path); 
 //});
 app.get('/', function(req, res) {
-  res.sendfile('public_html/index.html');
+    res.sendfile('public_html/index.html');
 });
 
 app.get('/code.js', function(req, res) {
-  res.sendfile('public_html/code.js');
+    res.sendfile('public_html/code.js');
 });
 
 app.get('/style.css', function(req, res) {
-  res.sendfile('public_html/style.css');
+    res.sendfile('public_html/style.css');
 });
 
 app.get('/:index', function(req, res) {
-  res.sendfile('public_html/index.html');
+    res.sendfile('public_html/index.html');
 });
 
-app.get('/retrieve/:index', function(req,res) {
-    res.json(codes[req.params.index]);
+app.get('/retrieve/:index', function(req, res) {
+    if (codes[req.params.index])
+        res.json(codes[req.params.index]);
+    else
+        res.json("NOT FOUND");
 });
 
-app.post('/save', function(req,res) {
+app.post('/save', function(req, res) {
     console.log("SAVING::::");
-    codes[index] = req.body.code;
-    index += 1;
-    res.redirect('/' + (index-1));
+    codes[nextIndex] = req.body.code;
+    nextIndex += 1;
+    save();
+    res.redirect('/' + (nextIndex - 1));
 });
+
+var save = function() {
+    for (var i = 0; i < nextIndex; i++) {
+        var path = 'server_save/' + i;
+        fs.exists(path, function(exists) {
+            if (!exists) {
+                fs.writeFile(path, codes[i]);
+            }
+        });
+    }
+};
 
 var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var startListening = function() {
+    app.listen(port, function() {
+        console.log("Listening on " + port);
+    });
+};
+
+var loadFile = function(i) {
+    fs.readFile('server_save/' + i, function(err, data) {
+        if (err) {
+            console.log("Loaded " + (i-1) + " saved elements");
+            nextIndex = i;
+            startListening();
+        }
+        else {
+            codes[i] = data;
+            loadFile(i + 1);
+        }
+    });
+};
+loadFile(0);
